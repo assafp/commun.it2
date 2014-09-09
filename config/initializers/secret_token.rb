81f4a28c6e550ba744816fb579d71b9b518a1c37
Assaf Paneth <assafp@ftbpro.com>
2014-09-09 19:06:02 +0300

    Empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommunIt2::Application.config.secret_token = '6120df24ff5f5dccd38a2790252f9f8cb1e87dd1f5cd4b00aa4891d8e8d42e384cb8ab614137eeebed619666466410ffd5f0f7726716eafa5d29bd5c29aa4049'
